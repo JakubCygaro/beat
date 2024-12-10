b68c36f0a0a47c62adb0798f81abbdf487ea4226
JakubCygaro <robertrugala@gmail.com>
2024-12-10 13:47:53 +0100

    text centering works

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -11,6 +11,8 @@ const height = 300;
 const crcXTrans = 0;
 const crcYTrans = 0;
 
+const DISPLAY_DEBUG = false;
+
 SVG.on(document, 'DOMContentLoaded', async function() {
     let svg = SVG();
     draw = svg.addTo('#canvas').size(width, height)
@@ -45,9 +47,14 @@ function drawSegments() {
     segmentCount = items.length
     let angle = 360 / segmentCount;
     group.clear()
+    let hackBoxOpacity;
+    //if (DISPLAY_DEBUG)
+    //    hackBoxOpacity = 10;
+    //else
+    hackBoxOpacity = 0;
     group.rect(radius * 2, radius * 2).attr({
         transform: `translate(${-radius}, ${-radius})`,
-        opacity: 0
+        opacity: hackBoxOpacity
     });
     for (i = 0; i < segmentCount; i++){
         let x = radius / 2
@@ -61,18 +68,14 @@ function drawSegments() {
         group.path(`M0 0 ${angleOffsetX},${angleOffsetY}`).fill('#fff')
     }
     for (i = 0; i < segmentCount; i++){
-        let x = radius / 4
+        let x = radius / 2
         let y = 0
-        //x = (x * Math.cos(degreesToRadians(angle / 2))) - (y * Math.sin(degreesToRadians(angle / 2)))
-        //y = (y * Math.cos(degreesToRadians(angle / 2))) + (x * Math.sin(degreesToRadians(angle / 2)))
         let thisAngle = degreesToRadians((angle * i) + angle / 2)
-        //console.log(`x: ${x} y: ${y} thisAngle: ${thisAngle}`)
         let angleOffsetX = (x * Math.cos(thisAngle)) - (y * Math.sin(thisAngle))
         let angleOffsetY = (y * Math.cos(thisAngle)) + (x * Math.sin(thisAngle))
+        if(DISPLAY_DEBUG)
+            group.path(`M0 0 ${angleOffsetX},${angleOffsetY}`).fill('#AAAA00')
 
-        //console.log(`x: ${x} y: ${y} angleOX: ${angleOffsetX} angleOY: ${angleOffsetY}`)
-        console.log(`item: ${items[i]}`)
-        let text = group.text(items[i])
         let angle90 = degreesToRadians(90)
         perpX = (angleOffsetX  * Math.cos(angle90)) - (angleOffsetY  * Math.sin(angle90))
         perpY = (angleOffsetY  * Math.cos(angle90)) + (angleOffsetX   * Math.sin(angle90))
@@ -80,31 +83,25 @@ function drawSegments() {
             x: perpX,
             y: perpY,
         });
-        let path = `M${normalizedPerpendiciular.x * 10} ${normalizedPerpendiciular.y * 10} ${angleOffsetX + normalizedPerpendiciular.x * 10},${angleOffsetY + normalizedPerpendiciular.y * 10}`
-        group.path(path).attr({
-            stroke:'#00ff00',
-            transform:`translate(${normalizedPerpendiciular.x * 20},${normalizedPerpendiciular.y * 20})` 
-        })
-        text.path(path)
+        let moveFactor = -20.5;
+        let path = `M${normalizedPerpendiciular.x * moveFactor} ${normalizedPerpendiciular.y * moveFactor} ${angleOffsetX + normalizedPerpendiciular.x * moveFactor},${angleOffsetY + normalizedPerpendiciular.y * moveFactor}`
+        if (DISPLAY_DEBUG)
+            group.path(path).attr({
+                stroke:'#00ff00',
+            })
+        console.log(`item: ${items[i]}`)
+        let text = group
+            .text(items[i])
+            .path(path)
             .font({ 
-                size: 20.5, 
+                size: -moveFactor, 
                 family: 'Arial' 
             })
             .attr({
                 startOffset:"50%",
                 "text-anchor":"middle",
-                side: "right"
+                side: "left"
             })
-        //let text = group.text(items[i])
-        //                .font({
-        //                    size: 20,
-        //                    family: 'Arial'
-        //                })
-        //                .attr({
-        //                    transform: `translate(${angleOffsetX}, ${angleOffsetY})`,
-        //                    startOffset:"50%",
-        //                    "text-anchor":"middle"
-        //                })
     }
 }
 
@@ -123,17 +120,12 @@ function addItem(text) {
     console.log(items)
 }
 
-//function drawItems() {
-//    for (const item of items) {
-//        
-//    }
-//}
-
 function degreesToRadians(degrees)
 {
     var pi = Math.PI;
     return degrees * (pi/180);
 }
+
 function normalizeVector(vector) {
     let magnitude = Math.sqrt(vector.x * vector.x + vector.y * vector.y)
     return {
